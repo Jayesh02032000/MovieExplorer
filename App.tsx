513c47c9f186b39bc8d34c3b3bda573e0069c3a0
Jayesh <jayesh@archstore.in>
2026-07-08 16:48:23 +0530

    removed .env and console logs

--- App.tsx
+++ App.tsx
@@ -3,7 +3,5 @@ import 'react-native-gesture-handler';
 import StackNavigator from "./src/navigation/StackNavigator";
 
 export default function App() {
-   console.log("API:", process.env.EXPO_PUBLIC_TMDB_API_KEY);
-  console.log("TOKEN:", process.env.EXPO_PUBLIC_TMDB_ACCESS_TOKEN);
   return <StackNavigator />;
 }
